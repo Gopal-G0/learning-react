9e8b263c8cb41543471f6a0edc46b5a4a7280c19
Gopal-G0 <heyitsmegopal@gmail.com>
2025-09-30 23:12:04 +0530

    started coding my character limit tracker project

---
+++ character-limit-tracker/CharacterInputCard.jsx
@@ -0,0 +1,26 @@
+export default function CharacterInputBox() {
+
+    const textBoxStyle = {
+
+        height: '30px',
+        width: '350px',
+        fontSize: '16px',
+        paddingTop: '15px',
+        paddingLeft: '8px',
+        borderRadius: '9px',
+        fontFamily: 'Fira Code'
+
+    }
+
+  return (
+    <div>
+
+      <div>
+
+        <textarea style = {textBoxStyle} placeholder="Start Typing..."></textarea>
+
+      </div>
+
+    </div>
+  );
+}
--- character-limit-tracker/src/App.jsx
+++ character-limit-tracker/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-
+import CharacterInputBox from "../CharacterInputCard"
 function App() {
 
 
   return (
-    <>
+    <div style = {{
+
+      display: 'flex',
+      gap: '8px',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: '90vh'
+
+    }}>
+
+      <h4>Text Box:</h4>
+      <CharacterInputBox />
       
-    </>
+    </div>
   )
 }
 
