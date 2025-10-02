70e7bced5716ce57107919d5af2ff417e1a7fcd5
Gopal-G0 <heyitsmegopal@gmail.com>
2025-10-02 21:14:02 +0530

    completed my project

--- character-limit-tracker/src/App.jsx
+++ character-limit-tracker/src/App.jsx
@@ -1,21 +1,64 @@
-import CharacterInputBox from "../CharacterInputCard"
+import { useState } from 'react'
+import CharacterInputBox from "./CharacterInputCard.jsx"
 function App() {
+
+  const [textValue, setTextValue] = useState("");
+  let charLimit = 10;
+  let characterLeft = charLimit - textValue.length;
+
+  let crossedCharLimit;
+
+  if (characterLeft >= 0) {
+
+    crossedCharLimit = false;
+
+  } else {
+
+    crossedCharLimit = true;
+  }
+
+
+  const textBoxStyle = {
+
+    height: '30px',
+    width: '350px',
+    fontSize: '16px',
+    paddingTop: '15px',
+    paddingLeft: '8px',
+    borderRadius: '9px',
+    color: !crossedCharLimit ? 'black' : 'red'
+  }
 
 
   return (
-    <div style = {{
+
+    <div style={{
 
       display: 'flex',
+      flexDirection: 'column',
       gap: '8px',
+      fontFamily: 'CaskaydiaMono Nerd Font',
+      fontSize: '20px',
       alignItems: 'center',
       justifyContent: 'center',
-      height: '90vh'
-
+      height: '90vh',
     }}>
 
-      <h4>Text Box:</h4>
-      <CharacterInputBox />
-      
+      <h4>Character Limit Tracker</h4>
+      <CharacterInputBox
+        value={textValue}
+        onChange={(e) => setTextValue(e.target.value)}
+        style={textBoxStyle} /
+
+      >
+
+      <div>
+
+        <p>Remaining Characters: {characterLeft}</p>
+
+        { crossedCharLimit && <p style={{color: 'red'}}>You crossed the character limit</p> }
+
+      </div>
     </div>
   )
 }
